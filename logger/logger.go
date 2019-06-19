aac3ffd98dc5403840e9e9adcd7ff23f4eba365e
BigCodilo <44414197+BigCodilo@users.noreply.github.com>
2019-06-19 09:32:58 +0300

    Update and rename logger/logger.go to  logger/logger.go
    Errir

--- logger/logger.go
+++ logger/logger.go
@@ -15,20 +15,17 @@ func SetLoggers() error{
 	fileInfo, err := os.OpenFile("logger/LogInfo.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil{
 		Info.SetOutput(os.Stdout)
-		return err
 	}
 	Info = log.New(fileInfo, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
 	fileError, err := os.OpenFile("logger/LogError.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil{
 		Error.SetOutput(os.Stdout)
-		return err
 	}
 	Error = log.New(fileError, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 	fileDebug, err := os.OpenFile("logger/LogGebug.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil{
 		Debug.SetOutput(os.Stdout)
-		return err
 	}
 	Debug = log.New(fileDebug, "DEBUG: ", log.Ldate|log.Ltime|log.Lshortfile)
-	return nil
+	return err
 }
